5505953b3f1894a8c82333c4c7b0c4cb871e8072
apurb <apurb512@gmail.com>
2024-05-14 21:05:25 +0530

    Initial Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,19 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+//    id("com.google.gms.google-services") // Apply the Google Services plugin
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.google.gms.google.services) apply false
+}
+
+
+
+buildscript {
+    repositories {
+        google()
+        // other repositories
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.1")
+    }
 }
